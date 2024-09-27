d115f047baf9470696fced3bd7d59a2c0bdfef1e
Gourav Rajput <goavrathod8788@gmail.com>
2024-09-27 14:38:09 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -1,20 +1,21 @@
-class Solution(object):
+class Solution:
     def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        pass
+        stack = []
+        mapping = {')': '(', '}': '{', ']': '['}
 
+        for char in s:
+            if char in mapping:
+                top_element = stack.pop() if stack else None
+                if mapping[char] != top_element:
+                    return False
+            else:
+                stack.append(char)
 
+        return len(stack) == 0
 
-
-
-
-
-    
-
-
-
-  
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.isValid("()"))
+    print(sol.isValid("()[]{}"))
+    print(sol.isValid("(]"))
 
